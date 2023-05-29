c1601dff6c4fabe255e57f1fd9818a490c22cc9b
luiandre13 <75158280+luiandre13@users.noreply.github.com>
2023-05-29 13:47:50 -0600

    Modal

--- frontend/src/components/Layouts/cuerpo.js
+++ frontend/src/components/Layouts/cuerpo.js
@@ -7,7 +7,7 @@ export default function Cuerpo({header,children,color}) {
             <div className="grid  grid-rows-[1fr,60px] lg:grid-rows-[1fr,70px]">
                 <div className=''>
                     <header >
-                        <div className="  py-2 px-8 bg-green-400 border-b-4  border-gray-700">
+                        <div className="  py-2 px-8 bg-green-200 border-b-4  border-gray-700">
                             {header}
                         </div>
                     </header>
--- frontend/src/pages/ModalComponent.js
+++ frontend/src/pages/ModalComponent.js
@@ -3,76 +3,47 @@ import React, { useState } from 'react';
 const ModalComponent = ({ closeModal }) => {
   return (
     <>
-      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+      <div className="justify-center items-center flex  fixed inset-0 z-50 outline-none focus:outline-none">
         <div className="relative w-auto my-6 mx-auto max-w-3xl">
-          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+          <div className="border-2 rounded-lg shadow-lg  flex flex-col w-full p-6 bg-white outline-none focus:outline-none">
             <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-              <h3 className="text-3xl font-semibold">model titulo</h3>
+              <h3 className="text-3xl text-gray-900 font-semibold">Proponer Tema</h3>
               <button
-                className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                className="p-1 ml-auto border-0  text-black  float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                 onClick={closeModal}
-              >
-                <span className=" text-green-900 opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">X</span>
+              > X
               </button>
             </div>
-            <form className="space-y-6" action="#">
-                                                <div>
-                                                <label
-                                                    htmlFor="email"
-                                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                                                >
-                                                    Your email
-                                                </label>
-                                                <input
-                                                    type="email"
-                                                    name="email"
-                                                    id="email"
-                                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                                                    placeholder="name@company.com"
-                                                    required
-                                                />
-                                                </div>
-                                                <div>
-                                                <label
-                                                    htmlFor="password"
-                                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900"
-                                                >
-                                                    Your password
-                                                </label>
-                                                <input
-                                                    type="password"
-                                                    name="password"
-                                                    id="password"
-                                                    placeholder="••••••••"
-                                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                                                    required
-                                                />
-                                                </div>
-                                                
-                                
-                            </form>
-            <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-              <button
-                className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={closeModal}
-              >
-                Close
-              </button>
+            <form className="space-y-2 w-96" action="#">
+                    <div>
+                        <label for="email" className="block mb-2 text-sm font-medium text-gray-900 ">Titulo de Tesis</label>
+                        <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Escáner y geolocalizador para automóviles y tractores" required/>
+                    </div>
+                    <div>
+                        <label for="password" className="block mb-2 text-sm font-medium text-gray-900 ">Descripcion</label>
+                        <input type="password" name="password" id="password" placeholder="El objetivo de la tesis es desarrollar un dispositivo electrónico, el dispositivo será un escáner y geolocalizador para automóviles y tractores esto usando una placa particle boron y conector obd2 usando el puente cambus." className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required/>
+                    </div>
+                    <div>
+                        <label for="email" className="block mb-2 text-sm font-medium text-gray-900 ">Numero de Alumnos</label>
+                        <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="2" required/>
+                    </div> 
+                </form>
+            <div className="flex items-center justify-end  border-t border-solid border-slate-200 rounded-b mt-6">
               <button
                 className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={closeModal}
-              >
-                Save Changes
+                type="button" onClick={closeModal}>
+                Guardar
               </button>
             </div>
           </div>
         </div>
       </div>
-      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+      <div className="opacity-30 fixed inset-0 z-40 bg-black"></div>
     </>
   );
 };
 
 export default ModalComponent;
+/*
+            
+             */
